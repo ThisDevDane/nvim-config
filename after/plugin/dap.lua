f4ea26d3169206cd740964e1145ce2fb475981c3
Mikkel Hjortshøj <mhj@scadaminds.com>
2023-06-16 17:51:31 +0200

    feat: add debugging for csharp

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,4 +1,89 @@
 local dap = require('dap')
+
+dap.adapters.coreclr = {
+    type = 'executable',
+    command = 'netcoredbg',
+    args = { '--interpreter=vscode' }
+}
+
+local function find_csharp_dll()
+    local expanded_path = vim.fn.expand("%:p:h")
+    local plenary_path = require("plenary.path")
+    local current_path = plenary_path:new(expanded_path)
+    local scan = require("plenary.scandir")
+
+    local idx = 0
+    local function look_for_csproj(path, recursion_depth)
+        if recursion_depth > 6 then
+            print("Reached maximum allowed recusion to find .csproj")
+            return plenary_path:new(), false
+        end
+        local found_csproj = false
+
+        local files = scan.scan_dir(path:expand(), { hidden = false, depth = 1 })
+        for _, file in pairs(files) do
+            if file:sub(-string.len(".csproj")) == ".csproj" then
+                found_csproj = true
+                return plenary_path:new(file), true
+            end
+        end
+
+        if found_csproj == false then
+            recursion_depth = recursion_depth + 1
+            path = path:parent()
+            return look_for_csproj(path, recursion_depth)
+        end
+    end
+    local csproj_path, found = look_for_csproj(current_path, idx)
+
+    if found then
+        local assemlby_name = ""
+        local framework = ""
+
+        local xml2lua = require("xml2lua")
+        local handler = require("xmlhandler.tree"):new()
+        local parser = xml2lua.parser(handler)
+        parser:parse(csproj_path:read())
+        local groups = handler.root.Project.PropertyGroup
+
+        if #groups < 1 then
+            groups = {groups, {}}
+        end
+
+        for _, p in pairs(groups) do
+            if p.TargetFramework ~= nil then
+                framework = p.TargetFramework
+            end
+            if p.AssemblyName ~= nil then
+                assemlby_name = p.AssemblyName
+            end
+        end
+
+        return csproj_path:parent():expand().."/bin/Debug/"..framework.."/"..assemlby_name..".dll"
+    end
+
+    return ""
+end
+
+dap.configurations.cs = {
+    {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+            local input = ''
+
+            vim.ui.input({ prompt = 'Path to dll: ', default = find_csharp_dll(), completion = 'file' },
+                function(i)
+                    input = i
+                end)
+            return input
+        end,
+    },
+}
+
+
+
 require('dap-go').setup()
 local ui = require("dapui")
 ui.setup()
@@ -6,22 +91,22 @@ ui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
     ui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-    ui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-    ui.close()
-end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--     ui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--     ui.close()
+-- end
 
 -- Symbols
 vim.api.nvim_set_hl(0, 'DapBreakpointRejected', { ctermbg = 0, fg = '#787878' })
-vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379'})
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379' })
 
-vim.fn.sign_define('DapBreakpoint', {text='●', texthl='DapBreakpoint', linehl='', numhl=''})
-vim.fn.sign_define('DapBreakpointCondition', {text='▲', texthl='DapBreakpointCondition', linehl='', numhl=''})
-vim.fn.sign_define('DapBreakpointRejected', {text='◐', texthl='DapBreakpointRejected', linehl='', numhl=''})
-vim.fn.sign_define('DapLogPoint', {text='◯', texthl='DapLogPoint', linehl='', numhl=''})
-vim.fn.sign_define('DapStopped', {text='▶', texthl='DapStopped', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '▲', texthl = 'DapBreakpointCondition', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpointRejected', { text = '◐', texthl = 'DapBreakpointRejected', linehl = '', numhl = '' })
+vim.fn.sign_define('DapLogPoint', { text = '◯', texthl = 'DapLogPoint', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '▶', texthl = 'DapStopped', linehl = '', numhl = '' })
 
 
 -- Keymaps
--- lua/thisdevdane/packer.lua
+++ lua/thisdevdane/packer.lua
@@ -12,114 +12,82 @@ end
 local packer_bootstrap = ensure_packer()
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use) -- Packer
-    use 'wbthomason/packer.nvim'
+return require('packer').startup({
+    function(use) -- Packer
+        use 'wbthomason/packer.nvim'
 
-    -- Treesitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = 'TSUpdate',
-    }
-    use 'nvim-treesitter/nvim-treesitter-context'
-    use {
-        'IndianBoy42/tree-sitter-just',
-        config = function()
-            require('tree-sitter-just').setup({})
-        end
-    }
-    use 'HiPhish/nvim-ts-rainbow2'
-
-    -- Language Servers
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {                            -- Optional
-                'williamboman/mason.nvim',
-                run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-buffer' },   -- Required
-            { 'hrsh7th/cmp-path' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-            { 'saadparwaiz1/cmp_luasnip' }
+        -- Treesitter
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = 'TSUpdate',
         }
-    }
+        use 'nvim-treesitter/nvim-treesitter-context'
+        use {
+            'IndianBoy42/tree-sitter-just',
+            config = function()
+                require('tree-sitter-just').setup({})
+            end
+        }
+        use 'HiPhish/nvim-ts-rainbow2'
 
-    use {
-        'folke/trouble.nvim',
-        requires = 'kyazdani42/nvim-web-devicons',
-        config = function()
-            require('trouble').setup()
-            vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>',
-                { silent = true, noremap = true }
-            )
-        end
-    }
+        -- Language Servers
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v2.x',
+            requires = {
+                -- LSP Support
+                { 'neovim/nvim-lspconfig' }, -- Required
+                {                            -- Optional
+                    'williamboman/mason.nvim',
+                    run = ":MasonUpdate"
+                },
+                { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-    -- DAP
-    use 'mfussenegger/nvim-dap'
-    use 'rcarriga/nvim-dap-ui'
-    use 'leoluz/nvim-dap-go'
+                -- Autocompletion
+                { 'hrsh7th/nvim-cmp' },     -- Required
+                { 'hrsh7th/cmp-buffer' },   -- Required
+                { 'hrsh7th/cmp-path' },     -- Required
+                { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+                { 'L3MON4D3/LuaSnip' },     -- Required
+                { 'saadparwaiz1/cmp_luasnip' }
+            }
+        }
 
-    -- Theme/UI
-    use {
-        'catppuccin/nvim',
-        as = 'catppuccin',
-    }
+        use {
+            'folke/trouble.nvim',
+            requires = 'kyazdani42/nvim-web-devicons',
+            config = function()
+                require('trouble').setup()
+                vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>',
+                    { silent = true, noremap = true }
+                )
+            end
+        }
 
-    use 'kyazdani42/nvim-web-devicons'
-    use 'kyazdani42/nvim-tree.lua'
-    use 'hoob3rt/lualine.nvim'
-    use {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-        end
-    }
+        -- DAP
+        use 'mfussenegger/nvim-dap'
+        use 'rcarriga/nvim-dap-ui'
+        use 'leoluz/nvim-dap-go'
 
-    -- Utilities
-    use {
-        "klen/nvim-test",
-        config = function()
-            require('nvim-test').setup()
-        end
-    }
-    use 'lukas-reineke/indent-blankline.nvim'
-    use 'voldikss/vim-floaterm'
-    use 'jeffkreeftmeijer/vim-numbertoggle'
-    use 'unblevable/quick-scope'
-    use {
-        'takac/vim-hardtime', -- see http://vimcasts.org/blog/2013/02/habit-breaking-habit-making
-        config = function()
-            vim.g.hardtime_default_on = 0
-            vim.g.hardtime_showmsg = 1
-        end
-    }
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end
-    }
+        -- Theme/UI
+        use {
+            'catppuccin/nvim',
+            as = 'catppuccin',
+        }
 
-    use {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
+        use 'kyazdani42/nvim-web-devicons'
+        use 'kyazdani42/nvim-tree.lua'
+        use 'hoob3rt/lualine.nvim'
+        use {
+            'lewis6991/gitsigns.nvim',
+            config = function()
+                require('gitsigns').setup()
+            end
+        }
+
+        -- Utilities
+        use_rocks "xml2lua"
+        use { "nvim-lua/plenary.nvim" }
         use {
             "nvim-neotest/neotest",
             requires = {
@@ -127,16 +95,50 @@ return require('packer').startup(function(use) -- Packer
                 { "Issafalcon/neotest-dotnet" },
             }
         }
-        end
-    }
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-        },
-    }
+        use 'lukas-reineke/indent-blankline.nvim'
+        use 'voldikss/vim-floaterm'
+        use 'jeffkreeftmeijer/vim-numbertoggle'
+        use 'unblevable/quick-scope'
+        use {
+            'takac/vim-hardtime', -- see http://vimcasts.org/blog/2013/02/habit-breaking-habit-making
+            config = function()
+                vim.g.hardtime_default_on = 0
+                vim.g.hardtime_showmsg = 1
+            end
+        }
+        use {
+            'numToStr/Comment.nvim',
+            config = function()
+                require('Comment').setup()
+            end
+        }
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+        use {
+            "folke/which-key.nvim",
+            config = function()
+                vim.o.timeout = true
+                vim.o.timeoutlen = 300
+                require("which-key").setup {
+                    -- your configuration comes here
+                    -- or leave it empty to use the default settings
+                    -- refer to the configuration section below
+                }
+            end
+        }
+        use {
+            'nvim-telescope/telescope.nvim',
+            requires = {
+                'nvim-lua/plenary.nvim',
+            },
+        }
+
+        if packer_bootstrap then
+            require('packer').sync()
+        end
+    end,
+    config = {
+        luarocks = {
+            python_cmd = 'python3'
+        }
+    }
+})
