424a6dd2553ac7af0b4db5d417372f8f0c12731a
Mikkel Hansen <mhj@scadaminds.com>
2023-07-03 13:38:20 +0200

    feat(floatterm): add lazydocker as a shortcut

--- after/plugin/floatterm.lua
+++ after/plugin/floatterm.lua
@@ -2,5 +2,7 @@ vim.keymap.set('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 -
     { desc = '[FT] New terminal' })
 vim.keymap.set('n', '<leader>lg', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazygit<CR>',
     { desc = '[FT] LazyGit' })
+vim.keymap.set('n', '<leader>ld', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazydocker<CR>',
+    { desc = '[FT] LazyDocker' })
 vim.keymap.set('n', '<leader>k9', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 k9s<CR>',
     { desc = '[FT] k9s' })
