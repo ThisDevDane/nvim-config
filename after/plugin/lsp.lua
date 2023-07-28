c1ed8270c9700cd9d84498b2c25388aa39371fc8
Mikkel Hansen <mhj@scadaminds.com>
2023-07-28 14:51:57 +0200

    feat(lsp): add signature help completion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -100,6 +100,7 @@ require('luasnip.loaders.from_vscode').lazy_load()
 cmp.setup({
     sources = {
         { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
         { name = 'treesitter' },
         { name = 'buffer',    keyword_length = 3 },
         { name = 'luasnip',   keyword_length = 2 },
