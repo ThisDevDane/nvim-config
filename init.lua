fdccb2226d3e43e763a8951f5b9d3aaa1aec8717
Mikkel Hansen <mhj@scadaminds.com>
2023-09-11 21:20:21 +0200

    feat: setup path for local luarocks as packer is broken

--- init.lua
+++ init.lua
@@ -1,1 +1,17 @@
+local home = os.getenv("HOME")
+
+if home ~= nil then
+    local path = table.concat({
+        '~/.luarocks/share/lua/5.1/?.lua',
+        '~/.luarocks/share/lua/5.1/?/init.lua'
+    }, ";")
+
+    local cpath = table.concat({
+        '~/.luarocks/lib/lua/5.1/?.so'
+    }, ";")
+    package.path = package.path .. ';' .. path:gsub("~", home)
+    package.cpath = package.cpath .. ';'.. cpath:gsub("~", home)
+end
+
 require('thisdevdane')
+
