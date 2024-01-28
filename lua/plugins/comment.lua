c313c761cd61229ca2040acebd19f5ee4ebb7990
Mikkel Hansen <me@thisdevdane.com>
2024-01-28 17:09:33 +0100

    feat(mini): enable comment, hipatterns and tabline

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,5 @@
-return {
-    'numToStr/Comment.nvim',
-    lazy = false,
-    opts = {}
-}
+-- return {
+--     'numToStr/Comment.nvim',
+--     lazy = false,
+--     opts = {}
+-- }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,5 +4,8 @@ return {
     config = function()
         require('mini.surround').setup()
         require('mini.pairs').setup()
+        require('mini.comment').setup()
+        require('mini.hipatterns').setup()
+        require('mini.tabline').setup()
     end
 }
